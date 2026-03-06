bb6c9d566c5c7519915d01e9f45e70dbbde26047
Ryan <ryan.chattopadhyay1@gmail.com>
2026-03-06 05:03:39 -0700

    lmao

--- UAPC/twopointers/twopointeralgs.py
+++ UAPC/twopointers/twopointeralgs.py
@@ -23,3 +23,20 @@ for r in range(n):
 
     while condition_bad:
         l += 1
+
+# prefix sum
+prefix = [0]*(n+1)
+for i in range(n):
+    prefix[i+1] = prefix[i] + arr[i]
+
+sum_lr = prefix[r+1] - prefix[l]
+
+# binary search
+lo, hi = 0, 10**18
+
+while lo < hi:
+    mid = (lo + hi)//2
+    if feasible(mid):
+        hi = mid
+    else:
+        lo = mid + 1

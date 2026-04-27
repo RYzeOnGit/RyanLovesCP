c6328542dea2848384e88cb26d162fb98e33a3c5
Ryan <ryan.chattopadhyay1@example.com>
2026-04-27 01:39:34 -0600

    comments

--- Array&Hashing/Group anagram.py
+++ Array&Hashing/Group anagram.py
@@ -1,6 +1,10 @@
 # Hashmap to group anagrams using frequency
 # time complexity: O(n * m)
 # space complexity: O(n * m)
+# How we solve it:
+# 1. We create a hashmap to store the frequency of each character in the word
+# 2. We then add the word to the hashmap with the frequency as the key
+# 3. We then return the hashmap values
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         res = defaultdict(list)
